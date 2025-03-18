7516e0bdf55224470e9ab1d7a7678c7bb995974f
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2025-03-18 22:11:04 +0700

    methods

--- js/main.js
+++ js/main.js
@@ -104,31 +104,74 @@ Vue.component('kanban-board',{
           return this.columnNames[columnName];
       },
       formatDate(date){
-
+          if (!date) return '';
+          return new Intl.DateTimeFormat('ru',{
+              hour:'numeric',
+              minute:'numeric',
+              day:'numeric',
+              month:'numeric',
+              year:'numeric',
+          }).format(new Date(date));
       },
       createTask(){
-
+          const task = {
+              ...this.newTask,
+              createdAt:new Date(),
+              changedAt:new Date(),
+              isEditing:false,
+              isReturning:false,
+              returnCause:'',
+              editModel:{...this.newTask}
+          };
+          this.columns.planed.push(task);
+          this.isCreating = false;
+          this.newTask = {title: '', description:'',deadline:'',};
       },
       editTask(columnName, index){
-
+          this.columns[columnName][index].isEditing = true;
       },
       saveChanges(columnName, index){
-
+          const task = this.columns[columnName][index];
+          task.title = task.editModel.title;
+          task.description = task.editModel.description;
+          task.deadline = task.editModel.deadline;
+          task.changedAt = new Date();
+          task.isEditing = false;
       },
       deleteTask(columnName, index){
           this.columns[columnName].splice(index,1);
       },
       moveTask(fromColumn, index, toColumn, cause=null){
-
+          const task = this.columns[fromColumn].splice(index,1)[0];
+          if(toColumn === 'completed'){
+              task.isExpired = new Date() > new Date(task.deadline);
+          }
+          if(cause){
+              task.causes = task.causes || [];
+              task.causes.push(cause);
+          }
+          this.columns[toColumn].push(task);
       },
       returnTask(fromColumn, index, cause){
-
+          const task = this.columns[fromColumn][index];
+          if(!task){
+              console.error('Задача не найдена');
+              return;
+          }
+          this.moveTask(fromColumn,index,'inWork', cause);
+          if(task){
+              task.isReturning = false;
+          }
       },
       startDrag(index, columnName, event){
-
+          event.dataTransfer.setData('task-index', index);
+          event.dataTransfer.setData('from-column', columnName);
       },
       onDrop(toColumn, event){
-
+          const taskIndex = event.dataTransfer.getData('task-index');
+          const fromColumn = event.dataTransfer.getData('from-column');
+          if(toColumn === 'planed')return;
+          this.moveTask(fromColumn,taskIndex,toColumn);
       }
     },
 });
